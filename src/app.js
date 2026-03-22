46a3a34f510806fc94c81bd35e9a4bd7868dd68f
muhammedaslamshahs <muhammedaslamshahsofficieal@gmail.com.com>
2026-03-22 17:37:52 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import authRoutes from "./modules/auth/auth.routes.js";
+import productRoutes from "./modules/products/product.routes.js";
 import { errorHandler } from "./middleware/error.middleware.js";
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/products", productRoutes);
 
 app.get("/api/v1/health", (_req, res) => {
     res.json({ ok: true, message: "API is running..." });
